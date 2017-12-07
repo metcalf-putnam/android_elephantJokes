deca13d4246e4abbfae539ab379ee4b015c85728
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2017-12-06 21:32:52 -0500

    initial commit

---
+++ lamejokes/src/main/java/com/example/Jokes.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Jokes {
+    public String getJoke(){
+        return "How many ears does Spock have?\n" +
+                "Three. A left ear, a right ear and a Final Frontier";
+    }
+}
