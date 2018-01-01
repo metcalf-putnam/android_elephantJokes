9e9077d8ec799b9b208155a3cf618957479bee6e
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2018-01-01 12:39:06 -0500

    removed menu

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -21,35 +21,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new EndpointsAsyncTask(this).execute();
-        //new EndpointsAsyncTask(this).execute(new Pair<Context, String>(this, "Manfred"));
-       // mJokes = new Jokes();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void tellJoke(View view) {
-        //Toast.makeText(this, mJokes.getJoke(), Toast.LENGTH_SHORT).show();
         Context context = MainActivity.this;
         Class destinationActivity = JokeActivity.class;
         Intent jokeIntent = new Intent(context, destinationActivity);
@@ -60,10 +35,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onAsyncTaskComplete(String results) {
-        //make "tell joke" button visible (default should be gone), so cannot press button without results
         TextView button = findViewById(R.id.button_tell_joke);
         button.setVisibility(View.VISIBLE);
         mJokes = results;
-        //Toast.makeText(MainActivity.this, "I'm a puppy "  + results, Toast.LENGTH_SHORT).show();
     }
 }
